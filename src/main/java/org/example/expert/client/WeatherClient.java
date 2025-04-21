d3098b163e0f6ff6467188130e6c9c745e3fe6b3
exmrim <exmrim@naver.com>
2025-04-21 10:03:50 +0900

    Feature: //Lv1-2. 불필요한 if-else 피하기

--- src/main/java/org/example/expert/client/WeatherClient.java
+++ src/main/java/org/example/expert/client/WeatherClient.java
@@ -27,6 +27,15 @@ public class WeatherClient {
                 restTemplate.getForEntity(buildWeatherApiUri(), WeatherDto[].class);
 
         WeatherDto[] weatherArray = responseEntity.getBody();
+
+        //Lv1-2. 불필요한 if-else 피하기
+        if (!HttpStatus.OK.equals(responseEntity.getStatusCode())) {
+            throw new ServerException("날씨 데이터를 가져오는데 실패했습니다. 상태 코드: " + responseEntity.getStatusCode());
+        } else if(weatherArray == null || weatherArray.length == 0) {
+            throw new ServerException("날씨 데이터가 없습니다.");
+        }
+
+        /*
         if (!HttpStatus.OK.equals(responseEntity.getStatusCode())) {
             throw new ServerException("날씨 데이터를 가져오는데 실패했습니다. 상태 코드: " + responseEntity.getStatusCode());
         } else {
@@ -34,6 +43,7 @@ public class WeatherClient {
                 throw new ServerException("날씨 데이터가 없습니다.");
             }
         }
+        */
 
         String today = getCurrentDate();
 
